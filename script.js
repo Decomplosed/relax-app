65dafa7e19845f2facb9218c6ea2ae902b77b13b
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-28 10:56:48 +0200

    fixed typo

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const container = document.querySelector('.container')
 const text = document.querySelector('#text')
 
 const totalTime = 7500
-const breathTime = (totalTime / 5) * 2
+const breatheTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
 
 function breatheAnimation() {
   text.innerHTML = 'Breathe In!'
   container.className = 'container grow'
+
+  setTimeout(() => {}, breatheTime)
 }

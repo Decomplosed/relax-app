10e8989839bb19c98b47a75e0039fd098e843a43
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-28 10:56:05 +0200

    add container grow classes to container

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const text = document.querySelector('#text')
 const totalTime = 7500
 const breathTime = (totalTime / 5) * 2
 const holdTime = totalTime / 5
+
+function breatheAnimation() {
+  text.innerHTML = 'Breathe In!'
+  container.className = 'container grow'
+}
